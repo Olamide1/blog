fac2eb55bd89649222b8714c804cae1ee84ab22f
Olamide Akomolafe <olamideakomolafe1234@gmail.com>
2019-04-27 15:25:53 +0000

    Added facebook comments to the blog post and did finishing touches, TODO: properly design the comment box

---
+++ config/initializers/social_share_button.rb
@@ -0,0 +1,5 @@
+SocialShareButton.configure do |config|
+  config.allow_sites = %w(twitter facebook  weibo 
+                         email linkedin wechat
+                           reddit hacker_news odnoklassniki whatsapp_app whatsapp_web)
+end
